4dcee335a7a65bc531093e500b54f5828bf5ee42
Elizabeth Polgreen <epolgreen@gmail.com>
2021-01-11 19:54:59 +0000

    add more results

--- lib/run_cvc4_wg.sh
+++ lib/run_cvc4_wg.sh
@@ -1,11 +1,11 @@
 function run_cvc4 {
 for f in *.wg.sl; do
 
-if [ ! -f $f.cvc4_sygusrepair.output ]; then
+if [ ! -f $f.cvc4_sygusrepair_long2.output ]; then
 echo "processing benchmark $f with cvc4 plain"
-timeout 120 time ~/CVC4/build/bin/cvc4 --sygus-active-gen=enum --sygus-repair-const --sygus-repair-const-timeout=1000 --sygus-si=none --sygus-grammar-cons=any-const $f >> $f.cvc4_sygusrepair.output 2>&1
-timeout 120 time ~/CVC4/build/bin/cvc4 --sygus-active-gen=enum --sygus-si=none $f >> $f.cvc4_nosi.output 2>&1
-timeout 120 time ~/CVC4/build/bin/cvc4 --sygus-active-gen=enum --sygus-add-const-grammar --sygus-si=none $f >> $f.cvc4_const.output 2>&1
+timeout 1800 time ~/CVC4/build/bin/cvc4 --sygus-repair-const --sygus-repair-const-timeout=1000 --sygus-si=none --sygus-grammar-cons=any-const $f >> $f.cvc4_sygusrepair_long2.output 2>&1
+timeout 1800 time ~/CVC4/build/bin/cvc4  --sygus-si=none $f >> $f.cvc4_nosi_long2.output 2>&1
+timeout 1800 time ~/CVC4/build/bin/cvc4 --sygus-add-const-grammar --sygus-si=none $f >> $f.cvc4_const_long2.output 2>&1
 
 echo "done"
 fi
